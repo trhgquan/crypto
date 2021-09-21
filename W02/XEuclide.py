c0fdb641990452abe0c66c53a5d840bbf333bc8b
trhgquan <quanthcstpbt@gmail.com>
2021-09-21 20:23:12 +0700

    Update a class for XEuclide

--- W02/XEuclide.py
+++ W02/XEuclide.py
@@ -1,32 +1,41 @@
 from random import *
 import sys
 
-def egcd(a, b):
-  if a == 0:
-    return (b, 0, 1)
-  else:
-    g, y, x = egcd(b % a, a)
-    return (g, x - (b // a) * y, y)
+class Euclidean:
+  def __init__(self, a, b):
+    self.a = a
+    self.b = b
 
-def modinv(a, m):
-  g, x, y = egcd(a, m)
-  if g != 1:
-    return 'fail'
-  else:
+  def extended_gcd(self, a, b):
+    if a == 0:
+      return b, 0, 1
+
+    g, y, x = self.extended_gcd(b % a, a)
+    return g, x - (b // a) * y, y
+
+  def inverse_modulo(self, a, m):
+    g, x, y = self.extended_gcd(a, m)
+
+    if g != 1:
+      raise Exception('Inverse modular does not exist.')
     return x % m
 
-def solve(a, b):
-  print(a, b, egcd(a, b)[0], modinv(a, b), sep=',')
-    
+  def solve(self):
+    print('a =', self.a)
+    print('b =', self.b)
+
+    print('GCD(a, b) =', self.extended_gcd(self.a, self.b)[0])
+    print('a^-1 (mod b) =', self.inverse_modulo(self.a, self.b))
+
 def main():
-  with open('result.csv', 'w+') as f:
-    sys.stdout = f
+  a = int(input('a = '))
+  b = int(input('b = '))
 
-    for i in range(0, 100):
-      a = randrange(0, 1337)
-      b = randrange(0, 1337)
-
-    solve(a, b)
+  try:
+    test = Euclidean(a, b)
+    test.solve()
+  except Exception as message:
+    print(message)
 
 if __name__ == '__main__':
   main()
