af99b6a920d6c046583ee40cd8c27a441fa886cc
trhgquan <quanthcstpbt@gmail.com>
2021-10-02 15:56:43 +0700

    Added CRT, rename some folders

---
+++ XEuclidean/main.py
@@ -0,0 +1,24 @@
+from XEuclidean import *
+import random
+import sys
+
+def main():
+  # a = int(input('a = '))
+  # b = int(input('b = '))
+
+  # try:
+  #  XEuclidean().solve_plain(a, b)
+  # except Exception as message:
+  #  print(message)
+
+  with open('result.csv', 'w+') as f:
+    sys.stdout = f
+
+    for i in range(100):
+      a = random.randint(1, 1337)
+      b = random.randint(1, 1337)
+
+      XEuclidean().solve_csv(a, b)
+
+if __name__ == '__main__':
+  main()
